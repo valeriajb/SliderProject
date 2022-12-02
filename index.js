6f6a644829aed622677f308b43a4fa8e484628ef
Valeria Jiménez <vjimenezbedoya@gmail.com>
2022-12-01 21:55:22 -0500

    SliderFuncional

--- index.js
+++ index.js
@@ -2,34 +2,50 @@ const btnPrev = document.querySelector(".prev");
 const btnNext = document.querySelector(".next");
 const sliderContainer = document.querySelector(".slider");
 const imagesSlider = document.querySelectorAll(".image");
+const groupItems = document.querySelectorAll(".itemSlider");
+const itemSlider = document.querySelector(".itemSlider");
 let indexPhoto = 1;
 let numberImages = imagesSlider.length;
 
+const nextPhotoSlider = () => {
+  sliderContainer.style.transform = `translateX(-${indexPhoto * 800}px)`;
+  indexPhoto++;
+};
 
-const nextPhotoSlider=()=>{ 
-    sliderContainer.style.transform=`translateX(-${indexPhoto*800}px)`
-    indexPhoto++
-}
+const prevPhotoSlider = () => {
+  sliderContainer.style.transform = `translateX(-${(indexPhoto - 1) * 800}px)`;
+  indexPhoto--;
+};
+const getFirstPhoto = () => {
+  sliderContainer.style.transform = `translatex(0px)`;
+  indexPhoto = 1;
+};
+const getLastPhoto = () => {
+  sliderContainer.style.transform = `translatex(-${(indexPhoto + 3) * 800}px)`;
+  indexPhoto = 3;
+};
+btnNext.addEventListener("click", () => {
+  indexPhoto >= numberImages ? getFirstPhoto() : nextPhotoSlider();
+  console.log(indexPhoto);
+  console.log(numberImages);
+});
 
-const prevPhotoSlider=()=>{ 
-    sliderContainer.style.transform=`translateX(-${(indexPhoto-1)*800}px)`
-    indexPhoto--
-}
-const getFirstPhoto=()=>{
-    sliderContainer.style.transform=`translatex(0px)`
-    indexPhoto = 1;
-}
-const getLastPhoto=()=>{
-    sliderContainer.style.transform=`translatex(-${(3)*800}px)`
-    indexPhoto=3;
-}
-btnNext.addEventListener("click",()=>{
-    indexPhoto>=numberImages?getFirstPhoto():nextPhotoSlider()
-    console.log(indexPhoto)
-    console.log(numberImages)
+btnPrev.addEventListener("click", () => {
+  indexPhoto < 1 ? getLastPhoto() : prevPhotoSlider();
+});
 
-})
+const remove = () => {
+  groupItems.forEach((item) => {
+    item.style.backgroundColor = "transparent";
+  });
+};
 
-btnPrev.addEventListener("click",()=>{ 
-    indexPhoto<1?getLastPhoto():prevPhotoSlider()
-})
+{
+  groupItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      remove();
+      item.style.backgroundColor = "white";
+      sliderContainer.style.transform = `translateX(-${index * 800}px)`;
+    });
+  });
+}
